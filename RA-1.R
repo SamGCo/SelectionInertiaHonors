4b51ce7a6ef93c061ad016dd62a706a1c1baad0a
SamGCo <samgco11@gmail.com>
2022-10-31 14:32:41 -0400

    updated 10/31

--- RA-1.R
+++ RA-1.R
@@ -3,13 +3,19 @@ X2019_Risk_Adjustment <- read_excel("C:\\Users\\squam\\OneDrive\\Desktop\\Honors
 RA2019<-X2019_Risk_Adjustment #import data
 states<-unique(RA2019$STATE) #make list of states
 
-RAsum <- data.frame(matrix(ncol=2,nrow=0)) #create empty final data set 
-colnames(RAsum)<-c('State','RiskAdjustmentSum') #name columns for dataset
+RAsum <- data.frame(matrix(ncol=3,nrow=0)) #create empty final data set 
+colnames(RAsum)<-c('State','RiskAdjustmentSum','comp') #name columns for dataset
 tally=1 #tally for for loop
 for(x in states)
 {
+  comp=0
   currentRA<-subset(RA2019,STATE==x) #create dataframe of the x state
   currentValue<-sum(abs(currentRA$RA_IND))/2 #sum and divide by two RA
+  insurers<-currentRA$`HIOS INSURANCE COMPANY NAME` #create list of insurers for x state
+  for(i in insurers)
+  {
+    comp=comp+1 #for loop to create number of insurers per state
+  }
   RAsum[tally,]<-c(x,currentValue,comp) #add to data set
   tally=tally+1
 }
--- RA-2.R
+++ RA-2.R
@@ -1,8 +1,10 @@
 filingData2019 <- read.csv("C:\\Users\\squam\\OneDrive\\Desktop\\HonorsResearch\\2019FilingsData.csv")
+#incorporating demographic data into the RA data
+demoRA<-read_excel("C:\\Users\\squam\\OneDrive\\Desktop\\HonorsResearch\\2019 OEP State-Level Public Use File.xlsx",8)
 statesFiling<-unique(filingData2019$STATE)
 filingData2019<-subset(filingData2019,MARKET!="Small Group")
-plansByStates<- data.frame(matrix(ncol=2,nrow=0))
-colnames(plansByStates)<-c('State','PlanNum')
+plansByStates<- data.frame(matrix(ncol=11,nrow=0))
+colnames(plansByStates)<-c('State','PlanNum',"Age18","Age1825", "Age2634","Age3544","Age4554", "Age5564" ,"Age65",'Male','Female'  )
 
 #dataframe of number of markets per state
 
@@ -18,6 +20,7 @@ for(s in statesFiling)
   else{
   currentState<-subset(filingData2019,STATE==s)
   currentMkt<-subset(mktDf,mktStates==s)
+  currentDemo<-subset(demoRA,demoRA$'State Abbr.'==s)
   count=0
   plans<-currentState$PLAN_ID
   for(p in plans)
@@ -25,13 +28,24 @@ for(s in statesFiling)
     count=count+1
   }
   
-  plansByStates[forTally,]<-c(s,count/currentMkt$Markets)
+  plansByStates[forTally,]<-c(s,count/currentMkt$Markets,as.numeric(currentDemo$`Age < 18`),as.numeric(currentDemo$`Age 18-25`),as.numeric(currentDemo$`Age 26-34`),as.numeric(currentDemo$`Age 35-44`),as.numeric(currentDemo$`Age 45-54`),as.numeric(currentDemo$`Age 55-64`),as.numeric(currentDemo$`Age ≥65`),currentDemo$Male,currentDemo$Female)
   forTally<-forTally+1
   }
 }
 plansByStates$PlanNum<-as.numeric(plansByStates$PlanNum)
+plansByStates$Age18<-as.numeric(plansByStates$Age18)
+plansByStates$Age1825<-as.numeric(plansByStates$Age1825)
+plansByStates$Age2634<-as.numeric(plansByStates$Age2634)
+plansByStates$Age3544<-as.numeric(plansByStates$Age3544)
+plansByStates$Age4554<-as.numeric(plansByStates$Age4554)
+plansByStates$Age5564 <-as.numeric(plansByStates$Age5564)
+plansByStates$Age65 <-as.numeric(plansByStates$Age65)
+plansByStates$Male <-as.numeric(plansByStates$Male)
+plansByStates$Female <-as.numeric(plansByStates$Female)
+
 
 RAwithPlans<-merge(RAsum,plansByStates, by='State')
-comp.RA.lm<- lm(formula=RiskAdjustmentSum ~ PlanNum,data=RAwithPlans)
+RAwithPlans$comp<-as.numeric(RAwithPlans$comp)
+comp.RA.lm<- lm(formula=RiskAdjustmentSum ~ PlanNum+Age1825+ Age2634+Age3544+Age4554+Age5564+Age65+Male+comp,data=RAwithPlans)
 summary(comp.RA.lm)  
 
