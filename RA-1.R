8ef2b40460e9e61c87a5aa4b8dcb1e427eca4eeb
SamGCo <samgco11@gmail.com>
2023-01-11 10:35:40 -0500

    updated with hix plan data

--- RA-1.R
+++ RA-1.R
@@ -1,24 +1,36 @@
 library(readxl)
-X2019_Risk_Adjustment <- read_excel("C:\\Users\\squam\\OneDrive\\Desktop\\HonorsResearch\\RA\\2019 Risk Adjustment.xlsx")
-RA2019<-X2019_Risk_Adjustment #import data
-states<-unique(RA2019$STATE) #make list of states
-
-RAsum <- data.frame(matrix(ncol=3,nrow=0)) #create empty final data set 
-colnames(RAsum)<-c('State','RiskAdjustmentSum','comp') #name columns for dataset
+Risk_Adjustment <- read.csv("C:\\Users\\squam\\OneDrive\\Desktop\\HonorsResearch\\RA\\ra_reins.csv")
+ra2019<-read_xlsx("C:\\Users\\squam\\OneDrive\\Desktop\\HonorsResearch\\RA\\2019 Risk Adjustment.xlsx")
+RAData<-Risk_Adjustment #import data
+ra2019<-ra2019[1:7]
+colnames(ra2019)<-colnames(Risk_Adjustment)
+Risk_Adjustment<-rbind(Risk_Adjustment,ra2019)
+Risk_Adjustment<-subset(Risk_Adjustment,Year!=2014)
+states<-unique(RAData$STATE) #make list of states
+years<-unique(Risk_Adjustment$Year)
+RAsum <- data.frame(matrix(ncol=4,nrow=0)) #create empty final data set 
+colnames(RAsum)<-c('State','RiskAdjustmentSum','comp','year') #name columns for dataset
 tally=1 #tally for for loop
-for(x in states)
+insurersWithoutRA<-subset(Risk_Adjustment, RA_IND==0)
+Risk_Adjustment<-subset(Risk_Adjustment, RA_IND!=0)
+
+for(y in years)
 {
+  for(x in states){
   comp=0
-  currentRA<-subset(RA2019,STATE==x) #create dataframe of the x state
-  currentValue<-sum(abs(currentRA$RA_IND))/2 #sum and divide by two RA
-  insurers<-currentRA$`HIOS INSURANCE COMPANY NAME` #create list of insurers for x state
+  currentRA<-subset(Risk_Adjustment,STATE==x & Year==y) #create dataframe of the x state
+  currentValue<-sum(abs(as.numeric(currentRA$RA_IND)))/2 #sum and divide by two RA
+  insurers<-currentRA$HIOS #create list of insurers for x state
   for(i in insurers)
   {
     comp=comp+1 #for loop to create number of insurers per state
-  }
-  RAsum[tally,]<-c(x,currentValue,comp) #add to data set
+  }# remember to look at insurers per year
+  if(currentValue !=0){
+  RAsum[tally,]<-c(x,currentValue,comp,y) #add to data set
   tally=tally+1
+  }
+}
 }
 
 
-
+RAsum<-subset(RAsum, year==2015 | year==2016 | year==2017 | year==2018| year==2019)
--- RA-2.R
+++ RA-2.R
@@ -1,66 +1,107 @@
-filingData2019 <- read.csv("C:\\Users\\squam\\OneDrive\\Desktop\\HonorsResearch\\2019FilingsData.csv")
+filingData <- read.csv("C:\\Users\\squam\\OneDrive\\Desktop\\HonorsResearch\\acaData.csv")
 #incorporating demographic data into the RA data
-demoRA<-read_excel("C:\\Users\\squam\\OneDrive\\Desktop\\HonorsResearch\\2019 OEP State-Level Public Use File.xlsx",8)
-statesFiling<-unique(filingData2019$STATE)
-filingData2019<-subset(filingData2019,MARKET!="Small Group")
+demoRA<-totalDemo
+statesFiling<-unique(totalPlans$ST)
+#filingData<-subset(filingData, Year==2015 | Year==2016 | Year==2017 | Year==2018| Year==2019)
+#filingData<-subset(filingData,EXCHANGE=="Yes")
+#insurersFromFiling<-intersect(filingData$ISSUER_ID,insurersWithRA)
+#checking<-0
+# for(ye in insurersWithoutRA$Year){
+# for(i in insurersWithoutRA$HIOS)
+# {
+#   currentInsurer<-subset(insurersWithoutRA,HIOS==i & Year==ye)
+#   for(id in filingData$ISSUER_ID)
+#   {
+#     currentFiling<-subset(filingData,ISSUER_ID==id & Year==ye)
+#     if(nrow(currentFiling)==0){break}
+#     else if (currentFiling[1,]$ISSUER_ID==i)
+#     {
+#       filingData<-subset(filingData,ISSUER_ID!=id & Year!=ye)
+#       checking<-checking+1
+#     }
+#     else{break}
+#   }
+# }
+# }
 plansByStates<- data.frame(matrix(ncol=20,nrow=0))
-colnames(plansByStates)<-c('State','PlanNum','TotalEnrollees',"Age18","Age1825", "Age2634","Age3544","Age4554", "Age5564" ,"Age65",'Male','Female','Asian','AIAN','AA','White','NHPI','Multi','unknown','Other')
+colnames(plansByStates)<-c('State','PlanNum','TotalEnrollees',"Age18","Age1825", "Age2634","Age3544","Age4554", "Age5564" ,"Age65",'Male','Female','Asian','AIAN','AA','White','NHPI','Multi','unknown','Year')
 
 #creating dataframe of number of markets per state
 
 Markets<-c(13,3,7,7,19,9,8,1,1,67,16,1,6,13,17,7,7,8,8,4,4,7,16,9,6,10,4,4,4,1,6,5,8,16,4,17,5,7,9,1,46,4,8,27,6,1,12,9,11,16,3)
 mktStates<-(c("AL","AK", "AZ","AR","CA","CO" ,"CT","DE", "DC" , "FL","GA", "HI","ID", "IL","IN", "IA", "KS", "KY", "LA", "ME","MD","MA", "MI", "MN" ,"MS","MO","MT","NE","NV","NH", "NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"))
 mktDf<-data.frame(mktStates,Markets)
-popRA<-data.frame(matrix(ncol=3,nrow=0))
-colnames(popRA)<-c('State','RiskAdjustmentSum','comp')
+popRA<-data.frame(matrix(ncol=4,nrow=0))
+colnames(popRA)<-c('State','RiskAdjustmentSum','comp','Year')
+Years<-c(2015,2016,2017,2018,2019)
 forTally<-1
+for(y in Years)
+{
 for(s in statesFiling)
 {
+  
   if(s=='MA'||s=='WY'||s=='DE'||s=='NE'||s=='AK'||s=='VT'){} #dropping MA and VT because there is no Risk Adjustment Data on them, dropping the rest because there is no demographic data for them
   else{
-  currentState<-subset(filingData2019,STATE==s)
+  currentState<-subset(totalPlans,ST==s & YEAR==y)
   currentMkt<-subset(mktDf,mktStates==s)
-  currentDemo<-subset(demoRA,demoRA$'State Abbr.'==s)
-  forRA <-subset(RAsum,State==s)
-  popRA[forTally,]<-c(forRA$State,as.numeric(forRA$RiskAdjustmentSum)/as.numeric(currentDemo$`Total Number of Consumers Who Have Selected an Exchange Plan`),forRA$comp)
+  currentDemo<-subset(demoRA,demoRA$State==s& Year==y)
+  forRA <-subset(RAsum,State==s & year==y)
+  if ((dim(forRA)[1] == 0)||(dim(currentDemo)[1] == 0))
+  {
+    
+  }
+  else{
+  popRA[forTally,]<-c(forRA$State,as.numeric(forRA$RiskAdjustmentSum)/as.numeric(currentDemo$TotalEnrollees),forRA$comp,forRA$year)
   
   count=0
-  plans<-currentState$PLAN_ID
+  }
+  plans<-currentState$PLANID
   for(p in plans)
   {
     count=count+1 #creating a count of number of plans per state 
   }
-  
-  plansByStates[forTally,]<-c(s,count/currentMkt$Markets,currentDemo$'Total Number of Consumers Who Have Selected an Exchange Plan',as.numeric(currentDemo$'Age < 18'),as.numeric(currentDemo$'Age 18-25'),as.numeric(currentDemo$'Age 26-34'),as.numeric(currentDemo$'Age 35-44'),as.numeric(currentDemo$'Age 45-54'),as.numeric(currentDemo$'Age 55-64'),as.numeric(currentDemo$'Age ≥65'),currentDemo$Male,currentDemo$Female,as.numeric(currentDemo$Asian),as.numeric(currentDemo$'American Indian / Alaska Native'),as.numeric(currentDemo$'African-American'),as.numeric(currentDemo$White),as.numeric(currentDemo$'Native Hawaiian / Pacific Islander'),as.numeric(currentDemo$Multiracial),as.numeric(currentDemo$'Unknown Race'),as.numeric(currentDemo$'Other Race'))
+  if ((dim(currentDemo)[1] == 0))
+  {}
+  else{
+  plansByStates[forTally,]<-c(s,count/currentMkt$Markets,as.numeric(currentDemo$TotalEnrollees),as.numeric(currentDemo$Age18),as.numeric(currentDemo$Age1825),as.numeric(currentDemo$Age2634),as.numeric(currentDemo$Age3544),as.numeric(currentDemo$Age4554),as.numeric(currentDemo$Age5564),as.numeric(currentDemo$Age65),currentDemo$Male,currentDemo$Female,as.numeric(currentDemo$Asian),as.numeric(currentDemo$AIAN),as.numeric(currentDemo$AA),as.numeric(currentDemo$White),as.numeric(currentDemo$NHPI),as.numeric(currentDemo$Multi),as.numeric(currentDemo$unknown),y)
   forTally<-forTally+1
-  testPlan<-plansByStates
-  
+
   }
+  }
+  }
+
 }
 #changing each variable into a numeric from a character
 plansByStates$PlanNum<-as.numeric(plansByStates$PlanNum)
-plansByStates$Age18<-as.numeric(plansByStates$Age18)/as.numeric(plansByStates$TotalEnrollees)
-plansByStates$Age1825<-as.numeric(plansByStates$Age1825)/as.numeric(plansByStates$TotalEnrollees)
-plansByStates$Age2634<-as.numeric(plansByStates$Age2634)/as.numeric(plansByStates$TotalEnrollees)
-plansByStates$Age3544<-as.numeric(plansByStates$Age3544)/as.numeric(plansByStates$TotalEnrollees)
-plansByStates$Age4554<-as.numeric(plansByStates$Age4554)/as.numeric(plansByStates$TotalEnrollees)
-plansByStates$Age5564 <-as.numeric(plansByStates$Age5564)/as.numeric(plansByStates$TotalEnrollees)
-plansByStates$Age65 <-as.numeric(plansByStates$Age65)/as.numeric(plansByStates$TotalEnrollees)
-plansByStates$Male <-as.numeric(plansByStates$Male)/as.numeric(plansByStates$TotalEnrollees)
-plansByStates$Female <-as.numeric(plansByStates$Female)/as.numeric(plansByStates$TotalEnrollees)
+plansByStates$Age18<-as.numeric(plansByStates$Age18)
+plansByStates$Age1825<-as.numeric(plansByStates$Age1825)
+plansByStates$Age2634<-as.numeric(plansByStates$Age2634)
+plansByStates$Age3544<-as.numeric(plansByStates$Age3544)
+plansByStates$Age4554<-as.numeric(plansByStates$Age4554)
+plansByStates$Age5564 <-as.numeric(plansByStates$Age5564)
+plansByStates$Age65 <-as.numeric(plansByStates$Age65)
+plansByStates$Male <-as.numeric(plansByStates$Male)
+plansByStates$Female <-as.numeric(plansByStates$Female)
 
-plansByStates$Asian <-as.numeric(plansByStates$Asian)/as.numeric(plansByStates$TotalEnrollees)
-plansByStates$AIAN <-as.numeric(plansByStates$AIAN)/as.numeric(plansByStates$TotalEnrollees)
-plansByStates$AA <-as.numeric(plansByStates$AA)/as.numeric(plansByStates$TotalEnrollees)
-plansByStates$White <-as.numeric(plansByStates$White)/as.numeric(plansByStates$TotalEnrollees)
-plansByStates$NHPI <-as.numeric(plansByStates$NHPI)/as.numeric(plansByStates$TotalEnrollees)
-plansByStates$Multi <-as.numeric(plansByStates$Multi)/as.numeric(plansByStates$TotalEnrollees)
-plansByStates$unknown <-as.numeric(plansByStates$unknown)/as.numeric(plansByStates$TotalEnrollees)
-plansByStates$Other <-as.numeric(plansByStates$Other)/as.numeric(plansByStates$TotalEnrollees)
+plansByStates$Asian <-as.numeric(plansByStates$Asian)
+plansByStates$AIAN <-as.numeric(plansByStates$AIAN)
+plansByStates$AA <-as.numeric(plansByStates$AA)
+plansByStates$White <-as.numeric(plansByStates$White)
+plansByStates$NHPI <-as.numeric(plansByStates$NHPI)
+plansByStates$Multi <-as.numeric(plansByStates$Multi)
+plansByStates$unknown <-as.numeric(plansByStates$unknown)
+#plansByStates$Other <-as.numeric(plansByStates$Other)
 
 
-RAwithPlans<-merge(popRA,plansByStates, by='State')
+#RAwithPlans<-merge(popRA,plansByStates, by='State')
+RAwithPlans<-inner_join(popRA,plansByStates,by=c("State","Year"))
 RAwithPlans$comp<-as.numeric(RAwithPlans$comp)
 RAwithPlans$RiskAdjustmentSum<-as.numeric(RAwithPlans$RiskAdjustmentSum)
-plans.RA.lm<- lm(formula=RiskAdjustmentSum ~ PlanNum+Age18+Age1825+ Age2634+Age3544+Age4554+Age65+Male+Asian+AIAN+AA+NHPI+Multi+White+unknown+comp,data=RAwithPlans)
-summary(plans.RA.lm)  
+RAwithPlans$PlanNum2<-as.numeric(RAwithPlans$PlanNum)^2
+RAwithPlans<-subset(RAwithPlans,PlanNum!=0)
+plans.RA.lm<- lm(formula=RiskAdjustmentSum ~ PlanNum+PlanNum2+Age18+Age1825+ Age2634+Age3544+Age4554+Age65+Male+AIAN+AA+NHPI+Multi+White+unknown+factor(Year)-1,data=RAwithPlans)
+print(summary(plans.RA.lm)  )
+RA.LM.No2<- lm(formula=RiskAdjustmentSum ~ PlanNum+Age18+Age1825+ Age2634+Age3544+Age4554+Age65+Male+AIAN+AA+NHPI+Multi+White+unknown+factor(Year)+comp,data=RAwithPlans)
+#stargazer(plans.RA.lm, RA.LM.No2,type='text', title="Results", align=TRUE)
+#stargazer(RAwithPlans,type='text')
+  
